a106d7bf765ee6dad72d16938409989f975ba5bf
Theo Willimse <cc094593@BPDEV2000.NET>
2016-11-22 11:45:38 +0200

    Added logic for Deck and Dice to atleast get the Price and some sort of indication of Qty

--- MTGPriceCheckerSpike/Form1.Designer.cs
+++ MTGPriceCheckerSpike/Form1.Designer.cs
@@ -37,13 +37,15 @@
             this.lstCardResults = new System.Windows.Forms.ListView();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(39, 397);
+            this.button6.Location = new System.Drawing.Point(52, 489);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(267, 39);
+            this.button6.Size = new System.Drawing.Size(356, 48);
             this.button6.TabIndex = 18;
             this.button6.Text = "Underworld Connections";
             this.button6.UseVisualStyleBackColor = true;
@@ -51,18 +53,20 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(39, 529);
+            this.button7.Location = new System.Drawing.Point(52, 651);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(267, 39);
+            this.button7.Size = new System.Drawing.Size(356, 48);
             this.button7.TabIndex = 17;
             this.button7.Text = "SCG";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(39, 268);
+            this.button5.Location = new System.Drawing.Point(52, 330);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(267, 39);
+            this.button5.Size = new System.Drawing.Size(356, 48);
             this.button5.TabIndex = 16;
             this.button5.Text = "Luck Shack";
             this.button5.UseVisualStyleBackColor = true;
@@ -70,9 +74,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(39, 202);
+            this.button4.Location = new System.Drawing.Point(52, 249);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(267, 39);
+            this.button4.Size = new System.Drawing.Size(356, 48);
             this.button4.TabIndex = 15;
             this.button4.Text = "Top Deck";
             this.button4.UseVisualStyleBackColor = true;
@@ -80,9 +85,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(39, 331);
+            this.button3.Location = new System.Drawing.Point(52, 407);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(267, 39);
+            this.button3.Size = new System.Drawing.Size(356, 48);
             this.button3.TabIndex = 14;
             this.button3.Text = "Scry";
             this.button3.UseVisualStyleBackColor = true;
@@ -90,9 +96,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(39, 133);
+            this.button2.Location = new System.Drawing.Point(52, 164);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(267, 39);
+            this.button2.Size = new System.Drawing.Size(356, 48);
             this.button2.TabIndex = 13;
             this.button2.Text = "Sad Robot";
             this.button2.UseVisualStyleBackColor = true;
@@ -100,35 +107,50 @@
             // 
             // lstCardResults
             // 
-            this.lstCardResults.Location = new System.Drawing.Point(352, 71);
+            this.lstCardResults.Location = new System.Drawing.Point(469, 87);
+            this.lstCardResults.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lstCardResults.Name = "lstCardResults";
-            this.lstCardResults.Size = new System.Drawing.Size(267, 365);
+            this.lstCardResults.Size = new System.Drawing.Size(355, 448);
             this.lstCardResults.TabIndex = 12;
             this.lstCardResults.UseCompatibleStateImageBehavior = false;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(32, 24);
+            this.textBox1.Location = new System.Drawing.Point(43, 30);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(274, 20);
+            this.textBox1.Size = new System.Drawing.Size(364, 22);
             this.textBox1.TabIndex = 11;
             this.textBox1.Text = "ancestral-mask";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(39, 71);
+            this.button1.Location = new System.Drawing.Point(52, 87);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(267, 39);
+            this.button1.Size = new System.Drawing.Size(356, 48);
             this.button1.TabIndex = 10;
             this.button1.Text = "Dracoti";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(52, 568);
+            this.button8.Margin = new System.Windows.Forms.Padding(4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(356, 48);
+            this.button8.TabIndex = 19;
+            this.button8.Text = "Deck and Dice";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(669, 524);
+            this.ClientSize = new System.Drawing.Size(892, 645);
+            this.Controls.Add(this.button8);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button5);
@@ -138,6 +160,7 @@
             this.Controls.Add(this.lstCardResults);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -156,6 +179,7 @@
         private System.Windows.Forms.ListView lstCardResults;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button8;
 
     }
 }
